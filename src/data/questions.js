e263dac9d09d4093bd0a226ba1e89f17e752d905
Renato Fujimoto <renatofujimoto2@gmail.com>
2022-10-03 11:47:10 -0300

    first commit

---
+++ src/data/questions.js
@@ -0,0 +1,61 @@
+const data = [
+  {
+    question: 'O que é Vanilla JavaScript?',
+    options: [
+      'JavaScript puro',
+      'Uma biblioteca JavaScript',
+      'Um framework JavaScript',
+      'Um compilador de JavaScript'
+    ],
+    answer: 'JavaScript puro'
+  },
+  {
+    question: 'Com qual instrução declaramos uma constante em JavaScript?',
+    options: ['const', 'let', 'var', 'define'],
+    answer: 'const'
+  },
+  {
+    question: 'Qual dos tipos de dado a seguir não existe em JavaScript?',
+    options: ['string', 'number', 'boolean', 'float'],
+    answer: 'float'
+  },
+  {
+    question: 'Qual dos métodos a seguir seleciona um elemento?',
+    options: ['querySelector', 'parseInt', 'sort', 'reduce'],
+    answer: 'querySelector'
+  },
+  {
+    question:
+      'Qual destas propriedades da a quantidade de elementos de um array?',
+    options: ['qty', 'length', 'items', 'index'],
+    answer: 'length'
+  },
+
+  {
+    question: 'Qual regra altera a cor de um elemento em CSS?',
+    options: ['color', 'background-color', 'font-size', 'transition'],
+    answer: 'color'
+  },
+  {
+    question: 'Qual atributo adiciona um link para a tag a em HTML?',
+    options: ['alt', 'href', 'src', 'link'],
+    answer: 'href'
+  },
+  {
+    question: 'Qual tag cria um parágrafo no HTML?',
+    options: ['<p>', '<h1>', '<text>', '<ul>'],
+    answer: '<p>'
+  },
+  {
+    question: 'A posição que deixa um elemento fixo é a:',
+    options: ['static', 'absolute', 'fixed', 'relative'],
+    answer: 'fixed'
+  },
+  {
+    question: 'Para aumentar a fonte de um elemento no CSS,utilizamos:',
+    options: ['font', 'text-transform', 'font-size', 'hover'],
+    answer: 'font-size'
+  }
+]
+
+export default data
